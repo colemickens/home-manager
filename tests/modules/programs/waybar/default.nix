a08e6adfda223aa7a6561dfa00e3c86984dd2d9a
Nicolas Berbiche <nic.berbiche@gmail.com>
2020-06-11 17:47:54 -0400

    waybar: add module

---
+++ tests/modules/programs/waybar/default.nix
@@ -0,0 +1,7 @@
+{
+  waybar-systemd-with-graphical-session-target =
+    ./systemd-with-graphical-session-target.nix;
+  waybar-systemd-with-sway-target = ./systemd-with-sway-target.nix;
+  waybar-styling = ./styling.nix;
+  waybar-settings-complex = ./settings-complex.nix;
+}
---
+++ tests/modules/programs/waybar/systemd-with-graphical-session-target.nix
@@ -0,0 +1,28 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+  package = pkgs.writeScriptBin "dummy-waybar" "" // { outPath = "@waybar@"; };
+in {
+  config = {
+    programs.waybar = {
+      inherit package;
+      enable = true;
+      systemd.enable = true;
+    };
+
+    nmt = {
+      description =
+        "if withSystemdIntegration is set, verify the service is WantedBy sway-session.target";
+      script = ''
+        assertPathNotExists home-files/.config/waybar/config
+        assertPathNotExists home-files/.config/waybar/style.css
+
+        assertFileContent \
+          home-files/.config/systemd/user/waybar.service \
+          ${./systemd-with-graphical-session-target.service}
+      '';
+    };
+  };
+}
