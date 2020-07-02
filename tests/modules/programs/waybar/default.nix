6dfd3eed167652878d5652842247336cd16934b3
Nicolas Berbiche <nic.berbiche@gmail.com>
2020-07-02 19:31:54 -0400

    Add test case for @colemickens test configuration

--- tests/modules/programs/waybar/default.nix
+++ tests/modules/programs/waybar/default.nix
@@ -4,4 +4,6 @@
   waybar-systemd-with-sway-target = ./systemd-with-sway-target.nix;
   waybar-styling = ./styling.nix;
   waybar-settings-complex = ./settings-complex.nix;
+  # Broken configuration from https://github.com/rycee/home-manager/pull/1329#issuecomment-653253069
+  waybar-broken-settings = ./broken-settings.nix;
 }
